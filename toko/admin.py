c0d4c725aca8959ff3d59d9aba7d0e9c048a4dfd
DITO RAMADHANI <85610941+Ndong2511@users.noreply.github.com>
2022-08-03 09:40:22 +0700

    Delete toko directory

--- toko/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Pelanggan)
-admin.site.register(Produk)
-admin.site.register(Pesan)
-admin.site.register(PesanBarang)
-admin.site.register(AlamatPengirim)
